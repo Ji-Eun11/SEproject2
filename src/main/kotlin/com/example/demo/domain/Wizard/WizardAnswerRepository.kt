c7ea8e144017cdd87562a33294ee1c857cf8ba9d
sehyun <o.sehyun@icloud.com>
2025-11-19 15:00:41 +0900

    Wizard commit

--- src/main/kotlin/com/example/demo/domain/Wizard/WizardAnswerRepository.kt
+++ src/main/kotlin/com/example/demo/domain/Wizard/WizardAnswerRepository.kt
@@ -3,4 +3,5 @@ package com.example.demo.domain.wizard.repository
 import com.example.demo.domain.wizard.WizardAnswer
 import org.springframework.data.jpa.repository.JpaRepository
 
+// 답변 조회용 리포지토리
 interface WizardAnswerRepository : JpaRepository<WizardAnswer, Long>
