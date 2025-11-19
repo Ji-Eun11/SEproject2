4d536dbe63689b17892adc26956dcba8ebd7173f
sehyun <o.sehyun@icloud.com>
2025-11-19 14:42:00 +0900

    Add WizardTag enum for categorizing wizard attributes

---
+++ src/main/kotlin/com/example/demo/domain/Wizard/WizardTag.kt
@@ -0,0 +1,8 @@
+package com.example.demo.domain.wizard
+
+enum class WizardTag {
+    SIZE_SMALL, SIZE_MEDIUM, SIZE_LARGE,
+    ENERGY_HIGH, ENERGY_LOW,
+    DIST_NEAR, DIST_MID, DIST_FAR,
+    TYPE_NATURE, TYPE_CITY, TYPE_PRIVATE
+}
